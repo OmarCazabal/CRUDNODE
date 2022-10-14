23241b58837cb5b53ca396241b9a92c055e07b8d
OmarCazabal <cazabal.rosalez.omar.adriano@gmail.com>
2022-10-13 23:26:12 -0500

    CRUD

--- controllers/crud.js
+++ controllers/crud.js
@@ -15,12 +15,10 @@ exports.save = (req, res)=>{
 };
 
 exports.update = (req,res)=>{
-    console.log("Hola")
     const id = req.body.id_videogames;
     const videojuego = req.body.videojuego; 
     const tipo= req.body.tipo;
     conexion.query('update videogames set ? where id_videogames = ?',[{videojuego:videojuego, tipo:tipo}, id], (error, results)=>{
-        console.log(results)
         if(error){
             console.log(error); 
         }else{
